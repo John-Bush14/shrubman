f755b3aeb4de9acae97be761462a52a9dbcd1479
John-Bush14 <joppe@stuyck.be>
2025-12-29 02:28:53 +0100

    trying out shmem

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,38 @@
+#![allow(irrefutable_let_patterns)]
+
+
+use std::{ffi::OsString, io::{ErrorKind}};
+
+use shared_memory::{ShmemConf, ShmemError};
+
+mod shrubd;
+
+// flink used for shared memory
+const SHMEM_FLINK: &str = "/tmp/shared_shrubs";
+// environment variable used to make program act as daemon
+const SHRUBD_ENABLE_VAR: &str = "START_SHRUBD";
+
 fn main() {
+    if std::env::var_os(SHRUBD_ENABLE_VAR).unwrap_or(OsString::from("0")) == "1" {
+        return shrubd::main();
+    }
+
+    let shmem = match ShmemConf::new().flink(SHMEM_FLINK).open() {
+        Ok(m) => m,
+        Err(err) => {
+            match err {
+                ShmemError::LinkDoesNotExist => (),
+                ShmemError::LinkOpenFailed(err) if err.kind() == ErrorKind::NotFound  => (),
+                _ => eprintln!("Non-fatal error occured while trying to open shared memory: {}", err),
+            }
+
+            shrubd::start_shrubd();
+
+            ShmemConf::new().flink(SHMEM_FLINK).open().expect("Failed to open shared memory after shrubd has been started")
+        }
+    };
+}
+
+struct SharedMemory {
+    pid: u64
 }
---
+++ src/shrubd.rs
@@ -0,0 +1,71 @@
+use std::{env, error::Error, io::{self, Read, Stdout, Write}, process::{self, Stdio}, thread::sleep, time::Duration};
+
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+use shared_memory::{Shmem, ShmemConf, ShmemError};
+
+
+use crate::{SHMEM_FLINK, SHRUBD_ENABLE_VAR, SharedMemory};
+
+
+pub(super) fn start_shrubd() {
+    println!("Starting daemon...");
+
+    let self_path = env::current_exe().unwrap();
+
+     #[allow(clippy::zombie_processes)]
+    let daemon_ps = process::Command::new(self_path)
+        .current_dir("/")
+        .env(SHRUBD_ENABLE_VAR, "1")
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("Shrubd command failed to start.");
+
+    let mut buf = [0u8];
+    daemon_ps.stdout.unwrap().read_exact(&mut buf).expect("Daemon didn't return succes code?");
+
+    match StartupResult::try_from(buf[0]).expect("Daemon returned invalid result? (try restarting shrubd)") {
+        StartupResult::Ok => println!("Daemon started up succesfully!"),
+        StartupResult::LinkExists => eprintln!("Shared memory is already in use? continuing as if daemon was started up normally."),
+        StartupResult::UnknownError => panic!("Unknown error has occured while daemon was starting up.")
+    };
+}
+
+#[repr(u8)]
+#[derive(TryFromPrimitive, IntoPrimitive, Debug)]
+enum StartupResult {
+    Ok = 0,
+    UnknownError = 1,
+    LinkExists = 2
+}
+
+impl From<&Box<dyn Error>> for StartupResult {
+    fn from(err: &Box<dyn Error>) -> Self {
+        match err.downcast_ref::<ShmemError>() {
+            Some(ShmemError::LinkExists) => StartupResult::LinkExists,
+            Some(_) | None => StartupResult::UnknownError
+        }
+    }
+}
+
+pub(super) fn main() { 
+    let shmem = match startup() {
+        Ok(m) => m,
+        Err(err) => {
+            print!("{}", u8::from(StartupResult::from(&err)) as char);
+            panic!("Fatal error occured while starting up: {}", err)
+        }
+    };
+    
+    print!("{}", u8::from(StartupResult::Ok) as char);
+    let _ = io::stdout().flush();
+
+    sleep(Duration::from_secs(10));
+
+    shmem.as_ptr();
+}
+
+fn startup() -> Result<Shmem, Box<dyn Error>> {
+    let shmem = ShmemConf::new().flink(SHMEM_FLINK).size(size_of::<SharedMemory>()).create()?;
+
+    Ok(shmem)
+}
